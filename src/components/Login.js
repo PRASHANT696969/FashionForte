9491984e340e0f55f7ad550cc8bd631c1c20be49
PRASHANT696969 <patelprashant7984@gmail.com>
2023-12-13 00:26:10 +0530

    Login and Signup Page Vlidation Done!

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import Validate from '../utils/Validate'
+import { useRef } from 'react';
+import { useState } from 'react';
 
 
 const Login = () => {
+const[errorMessage , setErrorMessage] = useState(null);
+
+  const email= useRef(null);
+  const password = useRef(null);
+  const handleButtonClick = () =>{
+      
+       console.log(email.current.value);
+       console.log(password.current.value);
+      const message =  Validate (email.current.value, password.current.value);
+      setErrorMessage(message);
+
+
+       
+
+
+  }
   return (
     <>
     <div className='overflow-x-hidden '> 
@@ -11,28 +30,30 @@ const Login = () => {
   className='z-20 absolute top-0 overflow-x-hidden h-full w-[100%]'
    src="./imgs/Login_page.jpg" alt="img not found"  />
    
-     <form className='absolute text z-30 w-1/2 flex flex-col   gap-14 left-1/3 top-[9rem] ml-[22rem] '>
+     <form onSubmit ={(e) => e.preventDefault()}  className='absolute text z-30 w-1/2 flex flex-col   gap-14 left-1/3 top-[9rem] ml-[22rem] '>
      <h1 className='text-[17px]  font-serif mt-[3rem]'>LOG INTO YOUR ACCOUNT</h1>
      <div className=''>
-        <input type=" text" placeholder='Email address' className='p-2  rounded w-[350px]  border-b-4 top-' />
+        <input ref={email} type=" text" placeholder='Email address' className='p-2  rounded w-[350px]  border-b-4 top-' />
             
-        <input type=" password" placeholder='Password' className='p-2  rounded w-[350px] bg-transparent border-b-4 mt-[2rem] flex' />
-        <button className=' rounded-lg bg-black text-white w-[350px] font-serif border-2 p-2 hover:bg-yellow-500 mt-[4rem] '>LOG IN</button>
+        <input ref={password} type=" password" placeholder='Password' className='p-2  rounded w-[350px] bg-transparent border-b-4 mt-[2rem] flex' />
+       <p className='text-red-500 font-bold'>{errorMessage }</p>
+        <button   className=' rounded-lg bg-black text-white w-[350px] font-serif border-2 p-2 hover:bg-yellow-500 mt-[4rem] ' onClick={handleButtonClick}>LOG IN</button>
         </div>
         
-        <div className=' cursor-pointer flex flex-row font-serif mt-[-3rem] gap-5 font-semibold text-sm tracking-wide'>
+        <div className=' cursor-pointer flex flex-row font-serif mt-[-3rem] gap-5 font-semibold text-sm '>
          <p className='hover:text-orange-500'>Forgot Password?</p>
          <p className='hover:text-orange-500'>Reset Password?</p>
          </div>
 
-         <div className='font-serif  flex flex-row gap-24   w-[23rem] p-2 mt-[0.7rem]' >
+         <div className='font-serif  flex flex-row gap-24   w-[23rem] p-2' >
          <h2 className='uppercase '>Need An Account ? </h2>
           <button className='  font-bold hover:text-orange-500'> REGISTER</button>
          </div>
      </form>
      </div>
       
-    </>
+    </> 
+
   )
 }
 
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
+import { useRef } from 'react'
+import Validate from '../utils/Validate'
+
 
 const SignUp = () => {
-  return (
+  const[errorMessage , setErrorMessage] = useState(null);
+
+  const email= useRef(null);
+  const password = useRef(null);
+  const ConfirmPassword =useRef(null);
+  const MobileNumber =useRef(null);
+  const handleButtonClick = () =>{
+      
+    console.log(email.current.value);
+    console.log(password.current.value);
+    console.log(ConfirmPassword.current.value);
+    console.log(MobileNumber.current.value);
+
+   const message =  Validate (email.current.value, password.current.value,ConfirmPassword.current.value , MobileNumber.current.value);
+   setErrorMessage(message);
+  }
+  return (   
     <>
     <div className='overflow-x-hidden	 '> 
 
@@ -10,17 +30,17 @@ const SignUp = () => {
 className='z-20 absolute top-0 overflow-x-hidden h-full w-[100%]'
  src="./imgs/Login_page.jpg" alt="img not found"  />
  
-   <form className='absolute text z-30 w-1/2 flex flex-col   gap-12 left-1/3 top-[9rem] ml-[22rem] '>
+   <form onSubmit={(e) =>e.preventDefault() } className='absolute text z-30 w-1/2 flex flex-col   gap-10 left-1/3 top-[9rem] ml-[22rem] '>
    <h1 className='text-[19px]  font-serif mt-[3rem]  font-semibold  '>Create Your New Account</h1>
    <div className='w-[1rem] flex flex-row'>
-   <input type=" text" placeholder='First Name' className= '  rounded p-2 w-[150px]  border-b-4 top-' />
-   <input type=" text" placeholder='Last Name' className='  rounded p-2 ml-[2rem] w-[150px]  border-b-4 top-' />
+   <input type=" text" placeholder='First Name' className= '  rounded p-2 w-[150px]  border-b-4 top- mb-[1rem]' />
+   <input type=" text" placeholder='Last Name' className='  rounded p-2 ml-[2rem] w-[150px] mb-[1rem]  border-b-4 top-' />
    </div>
-      <input type=" text" placeholder='Email address' className=' rounded p-2  w-[350px] mt-[-3rem] border-b-4 top-' />
+      <input type=" text" ref={email} placeholder='Email address' className=' rounded p-2  w-[350px] mt-[-3rem] border-b-4 top-' />
           
-      <input type=" password" placeholder='Password' className=' rounded p-2 w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
-      <input type=" password" placeholder='Confirm Password' className='p-2  rounded w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
-      <input type="text" placeholder='Mobile Number (for order status update)' className='p-2  rounded w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
+      <input   ref={password} type="  password" placeholder='Password' className=' rounded p-2 w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
+      <input ref={ConfirmPassword} type=" password" placeholder='Confirm Password' className='p-2  rounded w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
+      <input ref={MobileNumber} type="text" placeholder='Mobile Number (for order status update)' className='p-2  rounded w-[350px] bg-transparent border-b-4  flex mt-[-3rem]' />
       <div className='flex flex-row gap-2 mt-[-2rem] font-serif'>
       <h3 className='mr-[2rem] font-serif'>Gender</h3>
       <input type="checkbox" name="Male" id="" />Male
@@ -28,9 +48,10 @@ className='z-20 absolute top-0 overflow-x-hidden h-full w-[100%]'
       </div>
       
 
-      <button className=' rounded-lg bg-black text-white w-[350px] font-serif  border-2 p-2 hover:bg-yellow-500 mt-[-2rem] '>CREATE ACCOUNT</button>
+      <button  onClick={handleButtonClick} className=' rounded-lg bg-black text-white w-[350px] font-serif  border-2 p-2 hover:bg-yellow-500 mt-[-2rem] '>CREATE ACCOUNT</button>
       
-      
+      <p className='text-red-500 font-bold '>{errorMessage }</p>
+
       <div className=' cursor-pointer flex flex-row font-serif mt-[-2rem] gap-7 font-semibold text-sm '>
       <p>Already a Customer?</p>
       <button className=' font-extrabold underline text-[1rem] hover:text-orange-500 '>Login</button>
@@ -42,6 +63,6 @@ className='z-20 absolute top-0 overflow-x-hidden h-full w-[100%]'
 
     </>
   )
-}
+  }
 
 export default SignUp
